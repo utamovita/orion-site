0650f7b7ec56db2c98b450d5d2652a404ce1b980
Jakub Nadstoga <nadstogakuba@gmail.com>
2024-07-21 13:06:20 +0200

    Config Footer (#7)
    * Added Footerv1 & imported global scsss to base-layout
    
    * Added Headerv1, header scss &  some css to global
    
    * Changes based on PR
    
    ---------
    
    Co-authored-by: Michał Kowalski <mkowalski@pandagroup.co>

--- src/design-system/layout/header/lang-switcher/lang-switcher.component.tsx
+++ src/design-system/layout/header/lang-switcher/lang-switcher.component.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./lang-switcher.module.scss";
 
 function LangSwitcher() {
-  return <div className={styles.wrapper}>Lang switcher</div>;
+  return <div className={styles.wrapper}>Lang switcher TODO</div>;
 }
 
 export { LangSwitcher };
