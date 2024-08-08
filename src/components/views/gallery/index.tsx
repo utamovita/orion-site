423c2d94bc6dd16145718ebd053a749a66bf10af
Poniks <nadstogakuba@gmail.com>
2024-08-09 00:06:48 +0200

    minor changes in gallery

---
+++ src/components/views/gallery/index.tsx
@@ -0,0 +1,11 @@
+import { GalleryBanner } from "./gallery-banner/gallery-banner.component";
+
+function GalleryPageView() {
+  return (
+    <>
+      <GalleryBanner />
+    </>
+  );
+}
+
+export { GalleryPageView };
--- src/pages/galeria.tsx
+++ src/pages/galeria.tsx
@@ -1,15 +1,20 @@
+import { GalleryPageView } from "@components/views/gallery";
 import { BaseLayout } from "@design-system/layout/base-layout.component";
 import { GetStaticPropsContext } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function GalleryPage() {
-  return <BaseLayout>galeria</BaseLayout>;
+  return (
+    <BaseLayout>
+      <GalleryPageView />
+    </BaseLayout>
+  );
 }
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
     props: {
-      ...(await serverSideTranslations(locale as string, ["common"])),
+      ...(await serverSideTranslations(locale as string, ["common", "gallery"])),
     },
   };
 }
