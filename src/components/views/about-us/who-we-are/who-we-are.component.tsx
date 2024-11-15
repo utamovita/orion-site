dc00e20be56150cee9437cc69f16f321c346ee94
Jakub Nadstoga <nadstogakuba@gmail.com>
2024-11-15 16:56:30 +0100

    O nas (#12)
    * New Section 'O nas'
    
    * Icons, partial resposive
    
    * full responsive
    
    * partial translation
    
    * changes due to PR

--- src/components/views/about-us/who-we-are/who-we-are.component.tsx
+++ src/components/views/about-us/who-we-are/who-we-are.component.tsx
@@ -1,19 +1,39 @@
 import styles from "./who-we-are.module.scss";
 import { useTranslation } from "next-i18next";
-import { Container, Stack } from "@design-system/layout/utilities";
+import { Container, FlexComponent } from "@design-system/layout/utilities";
+import PunctualityIcon from "../../../shared/icons/punctuality.icon";
+import SecurityIcon from "../../../shared/icons/security.icon";
+import PartnershipIcon from "../../../shared/icons/partnership.icon";
+import StarsIcon from "../../../shared/icons/stars.icon";
 
 function WhoWeAre() {
   const { t } = useTranslation("common");
   return (
     <div className={styles.wrapper}>
       <Container>
-        <Stack className={styles.content}>
-          <div className={styles.section}>TODO</div>
-          <div className={styles.section}>
+        <FlexComponent className={styles.content}>
+          <div className={styles.sectionIcons}>
+            <div className={styles.sectionIconsChild}>
+              <PunctualityIcon className={styles.svg} />
+              <p>{t("aboutUs.whoWeAre.punctuality")}</p>
+              <StarsIcon className={styles.svg_Stars} />
+            </div>
+            <div className={styles.sectionIconsChild}>
+              <SecurityIcon className={styles.svg} />
+              <p>{t("aboutUs.whoWeAre.security")}</p>
+              <StarsIcon className={styles.svg_Stars} />
+            </div>
+            <div className={styles.sectionIconsChild}>
+              <PartnershipIcon className={styles.svg} />
+              <p>{t("aboutUs.whoWeAre.partnership")}</p>
+              <StarsIcon className={styles.svgStars} />
+            </div>
+          </div>
+          <div className={styles.sectionWhoWeAre}>
             <h2 className={styles.title}>{t("aboutUs.whoWeAre.title")}</h2>
             <p className={styles.subtitle}>{t("aboutUs.whoWeAre.subtitle")}</p>
           </div>
-        </Stack>
+        </FlexComponent>
       </Container>
     </div>
   );
