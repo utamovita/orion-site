e5e90f221ff70836a606d0371d922ac5c56a9b04
Michał Kowalski <mkowalski@pandagroup.co>
2024-11-15 06:37:58 +0100

    formatter

--- src/components/views/documents/index.tsx
+++ src/components/views/documents/index.tsx
@@ -1,14 +1,14 @@
 import { MainBanner } from "./main-banner/main-banner.component";
 import { DocumentsSlider } from "./documents-slider/documents-slider.component";
-import {Container} from "@design-system/layout/utilities";
+import { Container } from "@design-system/layout/utilities";
 
 function DocumentsView() {
   return (
     <>
       <MainBanner />
-        <Container>
+      <Container>
         <DocumentsSlider />
-        </Container>
+      </Container>
     </>
   );
 }
--- src/pages/kariera.tsx
+++ src/pages/kariera.tsx
@@ -1,10 +1,14 @@
 import { BaseLayout } from "@design-system/layout/base-layout.component";
 import { GetStaticPropsContext } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import {CareerView} from "@components/views/career";
+import { CareerView } from "@components/views/career";
 
 export default function CareerPage() {
-  return <BaseLayout><CareerView/></BaseLayout>;
+  return (
+    <BaseLayout>
+      <CareerView />
+    </BaseLayout>
+  );
 }
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
