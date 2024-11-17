d0c395efb5589cce647bc0cea258c7958cc75ea5
Poniks <nadstogakuba@gmail.com>
2024-11-17 22:06:54 +0100

    added EN/DE translation

--- src/components/views/career/work/work.component.tsx
+++ src/components/views/career/work/work.component.tsx
@@ -1,5 +1,5 @@
 import styles from "./work.module.scss";
-// import { useTranslation } from "next-i18next";
+import { useTranslation } from "next-i18next";
 import backgroundImage from "../../../../../public/assets/images/career/work.jpg";
 import { Container, FlexComponent } from "@design-system/layout/utilities";
 import { Button } from "../../../shared/button/button.component";
@@ -7,7 +7,7 @@ import { ArrowIcon } from "../../../shared/icons/arrow.icon";
 import Image from "next/image";
 
 function Work() {
-  //   const { t } = useTranslation("common");
+  const { t } = useTranslation("common");
 
   return (
     <div className={styles.wrapper}>
@@ -15,29 +15,26 @@ function Work() {
         <FlexComponent className={styles.flex}>
           <div className={styles.section}>
             <h2 className={styles.title}>
-              Praca w Grand <br></br>Transport Logistics
+              {t("career.subtitle.firstHalf")} <br></br>
+              {t("career.subtitle.secondHalf")}
             </h2>
-            <p className={styles.subtitle}>
-              Praca w Grand Transport Logistics to dla Ciebie idealna szansa na poszerzenie oferty transportowej lub
-              rozwinięcie swoich umiejętności spedycyjnych. Współpraca w projektach, atrakcyjny system wynagrodzenia czy
-              też stałe poszerzanie swoich umiejętności to tylko kilka cech, które charakteryzują naszą działalność.
-            </p>
-            <h2 className={styles.secondTitle}>Dostępne Stanowiska</h2>
+            <p className={styles.subtitle}>{t("career.text")}</p>
+            <h2 className={styles.secondTitle}>{t("career.work.title")}</h2>
             <div className={styles.buttons}>
               <Button className={styles.button}>
-                Przewoźnik <ArrowIcon className={styles.arrow} />
+                {t("career.work.carrier")} <ArrowIcon className={styles.arrow} />
               </Button>
               <Button className={styles.button}>
-                Kierowca <ArrowIcon className={styles.arrow} />
+                {t("career.work.driver")} <ArrowIcon className={styles.arrow} />
               </Button>
               <Button className={styles.button}>
-                Dyspozytor <ArrowIcon className={styles.arrow} />
+                {t("career.work.dispatcher")} <ArrowIcon className={styles.arrow} />
               </Button>
               <Button className={styles.button}>
-                Spedytor <ArrowIcon className={styles.arrow} />
+                {t("career.work.forwarder")} <ArrowIcon className={styles.arrow} />
               </Button>
               <Button className={styles.button}>
-                <span>Administracja</span> <ArrowIcon className={styles.arrow} />
+                <span>{t("career.work.administration")}</span> <ArrowIcon className={styles.arrow} />
               </Button>
             </div>
           </div>
